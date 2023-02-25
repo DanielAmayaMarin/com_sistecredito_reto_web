57167534bddcbd9b6a8ae6def8abfedaede07d24
DanielAmayaMarin <daniel.amayam@hotmail.com>
2023-02-25 14:13:00 -0500

    Validar existencia del subtotal

--- src/main/java/com/sistecredito/questions/ValidarSubtotal.java
+++ src/main/java/com/sistecredito/questions/ValidarSubtotal.java
@@ -1,13 +1,16 @@
 package com.sistecredito.questions;
 
 import static com.sistecredito.ui.UiCarrito.SUBTOTAL;
+import static com.sistecredito.ui.UiCarrito.SUBTOTAL_DOS;
+
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 
 public class ValidarSubtotal implements Question<Boolean> {
     @Override
     public Boolean answeredBy(Actor actor) {
-        return SUBTOTAL.resolveFor(actor).isVisible();
+
+        return SUBTOTAL.resolveFor(actor).isVisible() ? SUBTOTAL.resolveFor(actor).isVisible() : SUBTOTAL_DOS.resolveFor(actor).isVisible();
     }
 
     public static ValidarSubtotal enElCarrito(){

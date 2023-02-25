0a9b50019b3bce3dfd646781e21e96c9a22ff034
DanielAmayaMarin <daniel.amayam@hotmail.com>
2023-02-25 13:58:04 -0500

    Validar producto y cantidad

--- src/main/java/com/sistecredito/questions/ValidarProductos.java
+++ src/main/java/com/sistecredito/questions/ValidarProductos.java
@@ -1,5 +1,6 @@
 package com.sistecredito.questions;
 
+import com.sistecredito.interactions.ScrollElement;
 import com.sistecredito.utils.ArrayProductos;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
@@ -9,7 +10,19 @@ public class ValidarProductos implements Question<Boolean> {
     @Override
     public Boolean answeredBy(Actor actor) {
         String cantidadTotal = CANTIDADTOTAL.resolveFor(actor).isVisible() ? CANTIDADTOTAL.resolveFor(actor).getText().replace(" ( ", "").replace(" productos)", "").replace("(", "").trim() : CANTIDADTOTAL2.resolveFor(actor).getText().replace(" ( ", "").replace(" productos)", "").replace("(", "").trim();
-        return cantidadTotal.equals(ArrayProductos.getSumatoria());
+        boolean resultado = false;
+        if(cantidadTotal.equals(ArrayProductos.getSumatoria())){
+            for (int i = 0; i < ArrayProductos.getCantidad(); i++) {
+                actor.attemptsTo(
+                        ScrollElement.to(LABEL_TITULO(i+1))
+                );
+                System.out.println(LABEL_TITULO_TARGET(i+1).resolveFor(actor).isVisible());
+                if (LABEL_TITULO_TARGET(i).resolveFor(actor).isVisible()){
+                    resultado = true;
+                }
+            }
+        }
+        return cantidadTotal.equals(ArrayProductos.getSumatoria()) && resultado;
     }
 
     public static ValidarProductos enElCarrito() {
--- src/main/java/com/sistecredito/ui/UiCarrito.java
+++ src/main/java/com/sistecredito/ui/UiCarrito.java
@@ -6,7 +6,14 @@ import net.serenitybdd.screenplay.targets.Target;
 public class UiCarrito {
 
     public static final Target SUBTOTAL = Target.the("sub total").locatedBy("//span[@class=\"fb-order-status__sub-totals__value notranslate undefined\"]");
+    public static final Target SUBTOTAL_DOS = Target.the("sub total").locatedBy("//span[@data-testid=\"total-amount-SUB_TOTAL\"]");
     public static final Target CANTIDADTOTAL = Target.the("Label cantidad").locatedBy("//span[@class='count-number count notranslate']");
     public static final Target CANTIDADTOTAL2 = Target.the("Label cantidad ").locatedBy("//span[@data-testid='total-products-count']");
+    public static final String LABEL_TITULO (int id){
+        return "(//span[@data-testid='name'])["+id+"]";
+    }
+    public static final Target LABEL_TITULO_TARGET (int id){
+        return Target.the("Laberl Titulo").locatedBy("(//span[@data-testid='name'])['"+id+"']");
+    }
 
 }
